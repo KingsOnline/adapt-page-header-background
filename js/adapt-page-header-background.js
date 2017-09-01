d04bc0e78c49b9fa20181dc92598e0f7869bd16f
Simon  Date <simondate@yahoo.co.uk>
2017-09-01 15:22:32 +0100

    Changed to apply on postRender rather than on ready

--- js/adapt-page-header-background.js
+++ js/adapt-page-header-background.js
@@ -1,10 +1,8 @@
 define(["coreJS/adapt"], function(Adapt) {
-  Adapt.on("pageView:ready", function() {
+  Adapt.on("pageView:postRender", function() {
     var currentPageModel = Adapt.contentObjects._byAdaptID[Adapt.location._currentId][0];
-    console.log(currentPageModel);
     if (currentPageModel.get('_pageHeaderBackground') == undefined || currentPageModel.get('_pageHeaderBackground')._link == "")
       return;
-    console.log('applying');
     $('.page-header').css('background', 'url(' + currentPageModel.get('_pageHeaderBackground')._link + ')');
     $('.page-header').css('background-size', 'cover');
   });

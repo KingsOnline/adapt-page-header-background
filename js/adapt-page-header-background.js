c10019e32ec28a523ef330e4aad9f3a299dd250e
Simon  Date <simondate@yahoo.co.uk>
2018-01-25 10:49:02 +0000

    Changed attribute name so that it will work past AT bug

--- js/adapt-page-header-background.js
+++ js/adapt-page-header-background.js
@@ -1,9 +1,9 @@
 define(["coreJS/adapt"], function(Adapt) {
   Adapt.on("pageView:postRender", function() {
     var currentPageModel = Adapt.contentObjects._byAdaptID[Adapt.location._currentId][0];
-    if (currentPageModel.get('_pageHeaderBackground') == undefined || currentPageModel.get('_pageHeaderBackground')._link == "")
+    if (currentPageModel.get('_pageHeaderBackground') == undefined || currentPageModel.get('_pageHeaderBackground')._pageHeaderImage == "")
       return;
-    $('.page-header').css('background', 'url(' + currentPageModel.get('_pageHeaderBackground')._link + ')');
+    $('.page-header').css('background', 'url(' + currentPageModel.get('_pageHeaderBackground')._pageHeaderImage + ')');
     $('.page-header').css('background-size', 'cover');
   });
 

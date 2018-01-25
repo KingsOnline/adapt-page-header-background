417bee9bd5d054c82c3be259111a0b2626b5dade
Simon  Date <simondate@yahoo.co.uk>
2018-01-25 11:23:31 +0000

    Changed to `_graphic.src` as requested here https://github.com/adaptlearning/adapt_authoring/issues/1474

--- js/adapt-page-header-background.js
+++ js/adapt-page-header-background.js
@@ -1,9 +1,9 @@
 define(["coreJS/adapt"], function(Adapt) {
   Adapt.on("pageView:postRender", function() {
     var currentPageModel = Adapt.contentObjects._byAdaptID[Adapt.location._currentId][0];
-    if (currentPageModel.get('_pageHeaderBackground') == undefined || currentPageModel.get('_pageHeaderBackground')._pageHeaderImage == "")
+    if (currentPageModel.get('_pageHeaderBackground') == undefined || currentPageModel.get('_pageHeaderBackground')._graphic.src == "")
       return;
-    $('.page-header').css('background', 'url(' + currentPageModel.get('_pageHeaderBackground')._pageHeaderImage + ')');
+    $('.page-header').css('background', 'url(' + currentPageModel.get('_pageHeaderBackground')._graphic.src + ')');
     $('.page-header').css('background-size', 'cover');
   });
 

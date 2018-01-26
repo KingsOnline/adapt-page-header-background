664923bfc22d799306b8ca439c232cf67909d9ca
Simon  Date <simondate@yahoo.co.uk>
2018-01-26 10:28:48 +0000

    The extension now re-adds 'en/' in so that the image should load.

--- js/adapt-page-header-background.js
+++ js/adapt-page-header-background.js
@@ -3,7 +3,9 @@ define(["coreJS/adapt"], function(Adapt) {
     var currentPageModel = Adapt.contentObjects._byAdaptID[Adapt.location._currentId][0];
     if (currentPageModel.get('_pageHeaderBackground') == undefined || currentPageModel.get('_pageHeaderBackground')._link == "")
       return;
-    $('.page-header').css('background', 'url(' + currentPageModel.get('_pageHeaderBackground')._link + ')');
+      var e = currentPageModel.get('_pageHeaderBackground')._link;
+      e = e.slice(0, 7) + "en/" + e.slice(7);
+    $('.page-header').css('background', 'url(' + e + ')');
     $('.page-header').css('background-size', 'cover');
   });
 

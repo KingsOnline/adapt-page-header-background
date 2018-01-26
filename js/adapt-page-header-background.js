bbbce5dd737c03461007590fa598f501a0d328db
Simon  Date <simondate@yahoo.co.uk>
2018-01-26 10:18:01 +0000

    Changed attribute name again

--- js/adapt-page-header-background.js
+++ js/adapt-page-header-background.js
@@ -1,9 +1,9 @@
 define(["coreJS/adapt"], function(Adapt) {
   Adapt.on("pageView:postRender", function() {
     var currentPageModel = Adapt.contentObjects._byAdaptID[Adapt.location._currentId][0];
-    if (currentPageModel.get('_pageHeaderBackground') == undefined || currentPageModel.get('_pageHeaderBackground')._graphic.src == "")
+    if (currentPageModel.get('_pageHeaderBackground') == undefined || currentPageModel.get('_graphic').src == "")
       return;
-    $('.page-header').css('background', 'url(' + currentPageModel.get('_pageHeaderBackground')._graphic.src + ')');
+    $('.page-header').css('background', 'url(' + currentPageModel.get('_graphic').src + ')');
     $('.page-header').css('background-size', 'cover');
   });
 

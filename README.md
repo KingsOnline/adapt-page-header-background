8fb62845c7cf3f6d6f7f9c40849d86416c7edb03
simondate <simondate@yahoo.co.uk>
2018-04-11 12:39:11 +0100

    Fixed what was a broken plugin

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ adapt-menu-background
 ===============
 
 An extension that allows for a menu background with the [adapt-contrib-boxmenu](https://github.com/adaptlearning/adapt-contrib-boxmenu). This is just a stopgap until it gets added to Adapt Authoring Tool as an option.
+
+## Limitations
+
+Only works in AT version >= 0.3.1
--- js/adapt-page-header-background.js
+++ js/adapt-page-header-background.js
@@ -1,10 +1,9 @@
 define(["coreJS/adapt"], function(Adapt) {
   Adapt.on("pageView:postRender", function() {
-    var currentPageModel = Adapt.contentObjects._byAdaptID[Adapt.location._currentId][0];
-    if (currentPageModel.get('_pageHeaderBackground') == undefined || currentPageModel.get('_graphic').src == "")
+    var currentPageModel = Adapt.contentObjects._byAdaptID[Adapt.location._currentId][0].get('_pageHeaderBackground');
+    if (!currentPageModel || !currentPageModel.src)
       return;
-    $('.page-header').css('background', 'url(' + currentPageModel.get('_graphic').src + ')');
+    $('.page-header').css('background', 'url(' + currentPageModel.src + ')');
     $('.page-header').css('background-size', 'cover');
   });
-
 });
